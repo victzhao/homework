6360ab227bc63dc6a93ccc0f6ba6f078c6e88980
victzhao <zhaojianbosuper@163.com>
2015-12-29 23:14:00 +0800

    update by zhaojianbo

--- day1/Work2/china.py
+++ day1/Work2/china.py
@@ -14,6 +14,49 @@ def GetProvinceDic():
                 province_list.append(b[1])
             else:
                 pass
+    read_f.close()
     return province_dic
-print(GetProvinceDic())
+
+def GetCountryDic():
+    country_dic = {}
+    read_f = open('china.txt','r',encoding= 'utf8')
+    for i in read_f.readlines():
+        b = i.split()
+        if b[1] not in country_dic.keys():
+            country_list = [b[2]]
+            country_dic[b[1]] = country_list
+        else:
+            if b[2] not in country_dic[b[1]]:
+                country_list.append(b[2])
+            else:
+                pass
+    read_f.close()
+    return country_dic
+
+
+
+def GetProvinceOfNu(args,**kwargs):
+    NumOfProvinceDic={}
+    NumOfProvince = 0
+    for Province in args.keys():
+        NumOfProvince+=1
+        NumOfProvinceDic[NumOfProvince] = Province
+    return NumOfProvinceDic
+
+a = GetProvinceDic()
+
+print(a)
+
+
+
+
+
+def GetCountryOfNu(args):
+    pass
+
+
+
+
+
+
 
