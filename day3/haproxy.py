2a41d6271d98e0db156b5bc38a50990fe157f804
victzhao <zhaojianbosuper@163.com>
2016-01-20 00:00:27 +0800

    updat

--- day3/haproxy.py
+++ day3/haproxy.py
@@ -6,6 +6,7 @@
 import collections
 import json
 import sys
+import os
 #查找记录，通过用户给出的backend域名来查看其所有的记录
 def find(domain):
     FindOut = []
@@ -17,14 +18,48 @@ def find(domain):
                 card = True
             if card and line:
                 FindOut.append(line)
-            if not card and line.startswith('backend'):
-                card = False
+            if card and line.startswith('backend'):
+                pass
     return FindOut
 #添加记录
 def add(info):
-    print('add a recored')
+    add_domain = info['backend']
+    add_record = info['record']
+    find_record = find(add_domain)
+    if find_record:
+        with open('haproxy.conf','r') as read_file,open('haproxy.conf.new','w') as write_file:
+            card = False
+            add_backend = 'backend %s' %(add_domain)
+            for line in read_file:
+                line = line.strip('\n')
+                if add_backend == line:
+                    card = True
+                    continue
+                if card and line.startswith('backend'):
+                    card = False
+                    write_file.write('%s\n' %(line))
+                    continue
+                if not card and 'backend %s' %(add_domain) != line:
+                    write_file.write('%s\n' %(line))
+            add_record_strs  = '        server %s %s weitht %s maxconn %s' %(add_record['server'],add_record['server'],add_record['weight'],add_record['maxconn'])
+            find_record.append(add_record_strs)
+            for i in find_record:      #把需要添加的backend连同他的记录一起写进配置文件
+                write_file.write('%s\n'%i)
+    else:#backend不存在，需要添加backend和所有记录
+        pass
+
+
+
+
+
+
+
+
+
+
+
 def delete(info):
-    print('del a recored')
+     print('del a recored')
 
 def main():
     while True:
@@ -49,6 +84,7 @@ def main():
                         print(result)
                         break
                     else:
+                        print(result)
                         print('您查找的记录为空,请重新输入！')
                         continue
             #打印出查找结果
@@ -83,7 +119,7 @@ def main():
             sys.exit(0)
 
 if __name__ == '__main__':
-     main()
+       main()
 
 
 
