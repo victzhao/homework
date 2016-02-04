6812b12540a33158a781827084f47174229bb0d8
victzhao <zhaojianbosuper@163.com>
2016-02-04 22:00:44 +0800

    a

--- day4/compute.py
+++ day4/compute.py
@@ -13,7 +13,6 @@ __author__ = '赵建波'
          匹配加减的表达式，取出加减号左右两侧的数字（两侧的数字都包含+-号），对两侧的数字进行相加
 所有的计算流程：
           先找出要计算的表达式，用temp将其替换掉，再进行计算，然后把计算的结果换回temp，依次类推。
-
 函数说明：
     remove_space 去除空格
     compute_and_sub 计算加减法
@@ -23,9 +22,6 @@ __author__ = '赵建波'
     main 入口函数
 '''
 import re
-
-
-
 # 除去空格
 def remove_space(args):
     return  re.sub('\s','',args,count=0)
@@ -37,7 +33,6 @@ def compute_and_sub(args):
         return args
     FindAndSub = FindOut.group()
     Replaced  = args.replace(FindAndSub,'temp') #用临时temp替换查找到的加减表达式
-
     #GetInt = re.findall('\d+\.?\d*',FindMulDiv) #准备计算加减法,取出加减号左右两侧的数字
     GetInt = re.findall('[\+\-]{0,1}\d+\.?\d*',FindAndSub) #准备计算加减法,取出加减号左右两侧的数字
     Result = float(GetInt[0])+float(GetInt[1])
@@ -45,17 +40,14 @@ def compute_and_sub(args):
     args = Replaced.replace('temp',Result) #把计算出来的值替换回去
     args = remove_sub(args)
     return compute_and_sub(args) #递归依次重复处理
-
 #计算乘除，从左往右一次只计算一个表达式如2*3，首先找到乘法或者除法的表达式用temp将其从原表达式中替换，然后把计算的结果再替换回去，一直到得到一个只剩加减法的表达式
 def compute_mul_div(args):
     #FindOut = re.search('\d+\.?\d*[\/\*]\d+\.?\d*',args) #查找乘法或者除法,此处注意匹配小数点
     FindOut = re.search('\d+\.?\d*[\/\*][\+\-]{0,}\d+\.?\d*',args) #查找加法或者减法,此处注意匹配小数点
-
     if not FindOut:
         return args
     FindMulDiv = FindOut.group()
     Replaced  = args.replace(FindMulDiv,'temp') #用临时temp替换查找到的乘除表达式
-
     #GetInt = re.findall('\d+\.?\d*',FindMulDiv) #准备计算乘除法,取出乘除号左右两侧的数字
     GetInt = re.findall('[\+\-]{0,}\d+\.?\d*',FindMulDiv) #准备计算乘除法,取出乘除号左右两侧的数字
     GetLink = re.search('[\/\*]',FindMulDiv).group() #取出运算符号
@@ -67,7 +59,6 @@ def compute_mul_div(args):
     args = Replaced.replace('temp',Result) #把计算出来的值替换回去
     args = remove_sub(args)
     return compute_mul_div(args) #递归依次重复处理
-
 #合并正负号
 def remove_sub(args):
     args = args.replace('++','+')
@@ -75,7 +66,6 @@ def remove_sub(args):
     args = args.replace('--','+')
     args = args.replace('-+','-')
     return args
-
 #除去小括号
 def remove_brackets(args):
     args = remove_space(args)
@@ -90,8 +80,6 @@ def remove_brackets(args):
     result2 = result2.replace(')','') #把结果里的小括号去掉
     args = Replaced.replace('temp',result2) #用计算结果替换之前替换的temp
     return remove_brackets(args)
-
-
 def main(args):
     # #首先计算括号内的表达式
     r1 = remove_brackets(args)
@@ -102,7 +90,6 @@ def main(args):
     # #计算加减
     r4 = compute_and_sub(r3)
     print('本次计算结果为：%s' %r4)
-
 if __name__ == '__main__':
     expression = input('''\033[;35m++++++++++++++++++++++++++++++++++++++欢迎进入计算器系统++++++++++++++++++++++++++++++++++++++\033[0m
 \033[;32m本计算器可以计算类似这样的格式，您可以直接copy此表达式计算：
