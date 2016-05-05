8c45c0149c7a2a66d84e0e80c44b401643e21531
Zhaojianbo <zhaojianbosuper@163.com>
2016-05-05 18:39:54 +0800

    A

--- day15/django-pro/mysite/static/js/manager.js
+++ day15/django-pro/mysite/static/js/manager.js
@@ -1,6 +1,34 @@
 /**
  * Created by boboer on 2016/5/3.
  */
+//进入编辑模式
+
+
+
+
+function doEdit(tag){
+    tag.each(function(){
+        isEdited = $(this).attr("edit-enable");
+        if (isEdited == "true"){
+            currentVal=$(this).text();
+            temp='<input type="text" value="' + currentVal  + '" />'
+            $(this).html(temp);
+        };
+    });
+};
+
+//退出编辑模式
+function outEdit(tags){
+    tags.each(function(){
+        isEdited = $(this).attr("edit-enable");
+        if(isEdited=="true"){
+            currentVal=$(this).children().val()
+            $(this).html(currentVal)
+        }
+    });
+};
+
+
 //为每个菜单绑定事件并执行相应的动作
 $(function(){
     $(".left ul").children().each(function(){
@@ -13,52 +41,94 @@ $(function(){
 
 //全选
 function SelectAll(){
-    $(".tabletop input:checkbox ").prop("checked",true);
+    if($("#edit").hasClass("editing")){
+        $(".tabletop input:checkbox ").each(function(){
+            if ($(this).prop("checked")){
+
+            }else{
+                $(this).prop("checked",true);
+                tags=$(this).parent().siblings();
+                doEdit(tags)
+            };
+        });
+
+    }else{
+
+        $(".tabletop input:checkbox ").prop("checked",true);
+    }
+
 };
 function SelectNo(){
-    $(".tabletop input:checkbox ").prop("checked",false);
+    if ($("#edit").hasClass("editing")){
+        $(".tabletop input:checkbox ").each(function(){
+            if ($(this).prop("checked")){
+                $(this).prop("checked",false);
+                tags=$(this).parent().siblings();
+                outEdit(tags);
+            }
+        })
+    }else{
+        //直接取消选中
+        $(".tabletop input:checkbox ").prop("checked",false);
+    }
 };
 
 //反选
 function SelectSide(){
-    //isChecked=$(".tabletop input:checkbox ").prop("checked");
-    //if (isChecked){
-    //    $(".tabletop input:checkbox ").prop("checked",false);
-    //}else{
-    //    $(".tabletop input:checkbox ").prop("checked",true);
-    //}
-    $(".tabletop input:checkbox ").each(function(){
+    if($("#edit").hasClass("editing")){
+        //反选并进入编辑模式
+        $(".tabletop input:checkbox ").each(function(){
+        if ($(this).prop("checked")){
+            $(this).prop("checked",false);
+            tags=$(this).parent().siblings();
+            outEdit(tags);
+        }else{
+            $(this).prop("checked",true);
+            tags=$(this).parent().siblings();
+            doEdit(tags)
+            };
+        });
+        //进入编辑模式
+
+    }else{
+        //只反选
+        $(".tabletop input:checkbox ").each(function(){
         if ($(this).prop("checked")){
             $(this).prop("checked",false);
         }else{
             $(this).prop("checked",true);
-        };
-    });
+            };
+        });
+    }
 };
 
 //编辑模式
 function editMode(ths){
-    $(ths).css("background-color","red")
-    $(".tabletop input:checkbox ").each(function(){
+    if(!$(ths).hasClass("editing")){
+        $(ths).addClass("editing");
+        $(".tabletop input:checkbox ").each(function(){
         if ($(this).prop("checked")){
-            console.log("checked")
-            $(this).siblings().each(function(){
-                console.log($(this).text())
-                console.log("yes")
-                if ($(this).text()){
-                    var inner="<input  type='text' value=" + $(this).text() + ">";
-                    $(this).text("")
-                    $(this).append(inner)
-                    console.log("ok")
-                }
-            })
-        }
-    })
-}
+            tags=$(this).parent().siblings();
+            doEdit(tags);
+        };
+    });
+    }
+};
 
 //保存
 function Save(ths){
-    $(ths).prev().css("background-color","")
+    if($(ths).prev().hasClass("editing")){
+        $(ths).prev().removeClass("editing");
+        $(".tabletop input:checkbox ").each(function(){
+        if($(this).prop("checked")){
+            tags=$(this).parent().siblings();
+            outEdit(tags);
+            $(this).prop("checked",false)
+        }
+    })
+    }else{
+        alert("请进入编辑模式再操作！")
+    }
 };
 
 function addMode(){
@@ -91,4 +161,26 @@ function saveRecord(){
 function cleaning(){
     $("#add input:text").val("");
     $("#add input:text").removeClass("waring")
+}
+
+
+function SelectBox(ths){
+
+    if($("#edit").hasClass("editing")){
+        tags=$(ths).parent().siblings();
+        doEdit(tags);
+    };
+};
+
+function singleEdit(ths){
+    if(!$("#edit").hasClass("editing")){
+        $(ths).parent().siblings().first().children().prop("checked",true)
+        $("#edit").addClass("editing")
+        doEdit($(ths).parent().siblings());
+    }
+}
+
+
+function singleDelete(){
+
 }
